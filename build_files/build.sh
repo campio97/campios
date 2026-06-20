a45ed14961eb72e2942dadcfd6b2271a4a6604b0
Pietro Campiotti <pietro@campiotti.it>
2026-06-20 15:56:49 +0200

    Improved removing-apps flow

--- build_files/build.sh
+++ build_files/build.sh
@@ -39,11 +39,7 @@ $DNF install -y \
 $DNF install -y \
   nautilus \
   kitty \
-  gnome-terminal \
-  gnome-system-monitor \
-  gnome-calculator \
-  loupe \
-  mpv
+  loupe
 
 # ==========================================================
 # Niri
@@ -226,6 +222,7 @@ install -m 0755 /ctx/scripts/campios-install-flatpaks /usr/libexec/campios-insta
 
 install -d /usr/share/campios/flatpaks
 install -m 0644 /ctx/flatpaks/default-apps.txt /usr/share/campios/flatpaks/default-apps.txt
+install -m 0644 /ctx/flatpaks/remove-apps.txt /usr/share/campios/flatpaks/remove-apps.txt
 
 cat > /usr/lib/systemd/system/campios-install-flatpaks.service <<'EOF'
 [Unit]
@@ -237,18 +234,15 @@ ConditionPathExists=/usr/bin/flatpak
 [Service]
 Type=oneshot
 ExecStart=/usr/libexec/campios-install-flatpaks
+Restart=on-failure
+RestartSec=30
+TimeoutStartSec=15min
 
 [Install]
 WantedBy=multi-user.target
 EOF
 
 systemctl enable campios-install-flatpaks.service
-
-# Nel caso siano presenti come Flatpak system-wide
-flatpak uninstall --system -y --noninteractive \
-  org.mozilla.firefox \
-  org.mozilla.Thunderbird \
-  || true
 
 # ==========================================================
 # Schemi GLib
